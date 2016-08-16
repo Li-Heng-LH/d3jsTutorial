ac628cf29588c4cda8b3731634cc634eb7c009e7
Li Heng <liheng6567@gmail.com>
2016-08-16 17:14:42 +0800

    book 6.1 tutorial

---
+++ book6_1.js
@@ -0,0 +1,17 @@
+var dataset = [];
+for (var i = 0; i<25; i++){
+	var num = Math.floor(Math.random()*30);
+	dataset.push(num);
+}
+
+d3.select("body").selectAll("div")
+	.data(dataset)
+	.enter()
+	.append("div")
+	.attr("class", "bar")
+	.style("height", function(d){
+		return d*5 +"px";
+	});
+
+console.log(d3.selectAll("div")); //to see __data__
+console.log(dataset);
