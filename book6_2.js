63f02687aa4bd53a6b332ca0103d9b5b409250cb
Li Heng <liheng6567@gmail.com>
2016-08-16 17:52:27 +0800

    book6.2 till color

--- book6_2.js
+++ book6_2.js
@@ -1,5 +1,6 @@
 var w = 500;
 var h = 100;
+var barPadding = 1;
 var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
                 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
@@ -13,10 +14,14 @@ svg.selectAll("rect")
 	.enter()
 	.append("rect")
 	.attr("x",function(d,i){
-		return i*21;
+		return i*(w/dataset.length);
 	})
-	.attr("y",0)
-	.attr("width",20)
-	.attr("height",100)
-	.attr("fill", "blue");
+	.attr("y",function(d){
+		return h-d*4;
+	})
+	.attr("width",w/dataset.length-barPadding)
+	.attr("height",function(d){
+		return d*4;
+	})
+	.attr("fill", "teal");
 
