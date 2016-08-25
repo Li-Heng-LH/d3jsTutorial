73f361d9875a54772cf986b76d0261e2a5109b5f
Li Heng <liheng6567@gmail.com>
2016-08-25 17:46:25 +0800

    Done:chap7 scale Next:chapter8 Axes

--- book6_3.js
+++ book6_3.js
@@ -3,11 +3,35 @@
 
 var dataset = [
                 [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-              ]; //10
+                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
+                [600, 150]
+              ]; //11
 
 var w = 500;
-var h = 100;
+var h = 300;
+var padding = 20;
+
+var xScale = d3.scale.linear()
+					.domain ([0, d3.max(dataset,function(d){
+						return d[0];
+					})])
+					.range ([padding,w-padding*2])
+					.nice();
+
+var yScale = d3.scale.linear()
+					.domain ([0, d3.max(dataset,function(d){
+						return d[1];
+					})])
+					.range ([h-padding,padding])
+					.nice();
+
+//RMB, we want to set the radius according to the height of the circles
+var rScale = d3.scale.linear()
+					.domain ([0, d3.max(dataset,function(d){
+						return d[1]; //heigher the bigger
+					})])
+					.range ([2,5])
+					.nice();
 
 var svg = d3.select("body")
 			.append("svg")
@@ -19,12 +43,14 @@ svg.selectAll("circle")
 	.enter()
 	.append("circle")
 	.attr("cx",function(d){ //NOTE: each data entry here is an array
-		return d[0];
+		return xScale(d[0]);
 	})
 	.attr("cy",function(d){
-		return d[1];
+		return yScale(d[1]);
 	})
-	.attr("r",5);
+	.attr("r",function(d){
+		return rScale(d[1]);
+	});
 
 svg.selectAll("text")
 	.data(dataset)
@@ -34,12 +60,14 @@ svg.selectAll("text")
 		return d[0]+","+d[1];
 	})
 	.attr("x",function(d){
-		return d[0];
+		return xScale(d[0]);
 	})
 	.attr("y", function(d){
-		return d[1];
+		return yScale(d[1]);
 	})
 	.attr("font-size", "11px")
 	.attr("fill", "red")
 	.attr("font-family", "sans-serif");
+
+
 
